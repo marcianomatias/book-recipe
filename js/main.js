3e17df2d4fe8357df726010fac653aa59769f120
marcianomatias <marcianomatias1@hotmail.com>
2023-05-22 18:27:53 -0300

    :sparkles: feat: Adding JavaScript to project!

---
+++ js/main.js
@@ -0,0 +1,53 @@
+const API_KEY = "1b1bbbfb9a0246f0a74182245e1e1054";
+const recipeListEl = document.getElementById("recipe-list")
+
+function displayRecipes(recipes) {
+    recipeListEl.innerHTML = "";
+    recipes.forEach((recipe) => {
+        const recipeItemEl = document.createElement("li");
+        recipeItemEl
+            .classList
+            .add("recipe-item");
+        recipeImageEl = document.createElement("img");
+        recipeImageEl.src = recipe.image
+        recipeImageEl.alt = "recipe image";
+
+        recipeTitleEl = document.createElement("h2");
+        recipeTitleEl.innerText = recipe.title;
+
+        recipeIngredientsEl = document.createElement("p");
+        recipeIngredientsEl.innerHTML = `
+            <strong>Ingredients: </strong> ${recipe
+            .extendedIngredients
+            .map((ingredients) => ingredients.original)
+            .join(", ")}
+        `
+        recipeLinkEl = document.createElement("a");
+        recipeLinkEl.href = recipe.sourceUrl;
+        recipeLinkEl.innerText = "View Recipe";
+
+        recipeItemEl.appendChild(recipeImageEl);
+        recipeItemEl.appendChild(recipeTitleEl);
+        recipeItemEl.appendChild(recipeIngredientsEl);
+        recipeItemEl.appendChild(recipeLinkEl);
+        recipeListEl.appendChild(recipeItemEl);
+    })
+}
+async function getRecipes() {
+    const response = await fetch(
+        `https://api.spoonacular.com/recipes/random?number=15&apiKey=${API_KEY}`
+    )
+
+    const data = await response.json();
+
+    return data.recipes;
+
+};
+
+async function init() {
+    const recipes = await getRecipes();
+    displayRecipes(recipes);
+
+};
+
+init();
